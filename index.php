ca68401f1b9db3406549e533e2018f199b3cb927
shohruz93 <40568087+shohruz93@users.noreply.github.com>
2020-02-09 02:48:07 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
-<?php
-echo "hello word";
+<?php require_once "Alif.php"; ?>
+<html>
+<head>
+    <title> Welcome Alif </title>
+</head>
+<body>
 
-?>
+    <?php
+    $object = new Alif();
+    if (isset($_POST['saveFile'],$_POST['fileName'],$_POST['amal'])){
+        $fileName = htmlspecialchars($_POST['fileName']);
+        $amal = htmlspecialchars($_POST['amal']);
+        $object->amaliyat($fileName,$amal);
+    }
+    ?>
+
+ <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+     <select name="fileName">
+         <option value="shohruz">shohruz</option>
+     </select><br>
+    <input type="radio" name="amal" value="+"> +
+    <input type="radio" name="amal" value="-"> -
+    <input type="radio" name="amal" value="*"> *
+    <input type="radio" name="amal" value="/"> /
+    <br>
+    <input type="submit" name="saveFile" value="Send">
+ </form>
+</body>
+</html>
